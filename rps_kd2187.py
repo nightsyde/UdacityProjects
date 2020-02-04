3a28a9822ba62a835b57eafbcc39cea9b6355c66
nightsyde <nightside@iobbow.org>
2020-02-04 17:14:47 -0500

    UdacityRPSProject
    upload of Rock Paper Scissor project for review

---
+++ rps_kd2187.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+import random
+
+"""This program plays a game of Rock, Paper, Scissors between two Players,
+and reports both Player's scores each round."""
+
+moves = ['rock', 'paper', 'scissors']
+
+"""The Player class is the parent class for all of the Players
+in this game"""
+
+
+class Player:
+    def move(self):
+        return "rock"
+
+    def learn(self, my_move, their_move):
+        pass
+
+    def validateMove(self, response):
+        pass
+
+
+class RandomPlayer(Player):
+    def move(self):
+        return random.choice(moves)
+
+
+class HumanPlayer(Player):
+    def move(self):
+        index = 0
+        while index == 0:
+            response = input("Choose your move- rock, paper, or scissors: ")
+            if response in moves:
+                index = 1
+            else:
+                print(f"{response} is not a valid move. Please try again.\n")
+        return response
+
+
+class ReflectPlayer(Player):
+    def __init__(self):
+        self.lastmove = random.choice(moves)
+
+    def learn(self, my_move, their_move):
+        self.lastmove = their_move
+
+    def move(self):
+        return self.lastmove
+
+
+def beats(one, two):
+    return ((one == 'rock' and two == 'scissors') or
+            (one == 'scissors' and two == 'paper') or
+            (one == 'paper' and two == 'rock'))
+
+
+def whoWon(score1, score2):
+    # print(isinstance(score2, int))
+    print(f"{score1} to {score2}\n")
+    if score1 == score2:
+        print("It's a DRAW!!!")
+    elif score1 < score2:
+        print("Player 2 Wins!!!")
+    else:
+        print("Player 1 Wins!!!")
+
+
+class Game:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        self.p1Score = 0
+        self.p2Score = 0
+
+    def play_round(self):
+        move1 = self.p1.move()
+        self.p1.validateMove(move1)
+        move2 = self.p2.move()
+        print(f"Player 1: {move1}  Player 2: {move2}")
+        if (move1 == move2):
+            print("It's a DRAW! No score!")
+            print(f"Score is {self.p1Score} to {self.p2Score}\n")
+        elif (beats(move1, move2)):
+            print("Player 1 WINS")
+            self.p1Score += 1
+            print(f"Score is {self.p1Score} to {self.p2Score}\n")
+        else:
+            print("Player 2 WINS")
+            self.p2Score += 1
+            print(f"Score is {self.p1Score} to {self.p2Score}\n")
+        self.p1.learn(move1, move2)
+        self.p2.learn(move2, move1)
+
+    def play_game(self):
+        self.howMany = int(input("How many rounds do you want to play? "))
+        print("Game start!")
+        for round in range(self.howMany):
+            print(f"Round {round+1}:")
+            self.play_round()
+        whoWon(self.p1Score,self.p2Score)
+        print("Game over!")
+
+
+if __name__ == '__main__':
+    game = Game(HumanPlayer(), ReflectPlayer())
+    game.play_game()
