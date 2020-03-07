7b73c1932c3e6cbf416755596d8cc4af1794d612
nightsyde <nightside@iobbow.org>
2020-03-07 10:43:02 -0500

    Upload of final project - Pixel Art Maker

--- project-pixel-art-maker-kd2187/designs.js
+++ project-pixel-art-maker-kd2187/designs.js
@@ -1,43 +1,40 @@
+// Pixel Art Maker v1.0
+// Kirk Donaldson
 
 
-// When size is submitted by the user, call makeGrid()
-const form = document.getElementById('sizePicker');
-var dataSet = new FormData();
-var firstRun = 0;
+// declaring global varables
+const form = document.getElementById('sizePicker'); // form used to get gride size input
+var dataSet = new FormData(); // used to transfer form data
+var tdID = ""; // identifies individual cells
+const colorInput = document.getElementById('colorPicker'); // used to transfer color data
+const workSpace = document.getElementById('pixelCanvas'); // work space within page
+const gridSpace = document.createElement('table'); // table within workSpace
+gridSpace.setAttribute('id','gridSpace');
+workSpace.appendChild(gridSpace);
+var firstRun = 0; // check for first time page has run since refresh
 
-const workSpace = document.getElementById('pixelCanvas');
-// Select color input
-const colorInput = document.getElementById('colorPicker');
+// document.addEventListener('click', (e) => {
+//   console.log("mouse click");
+//
+// });
 
-document.addEventListener('click', function() {
-  console.log("mouse click");
-
-});
-function colorChanger(cell) {
-  var chosenCell = document.getElementById(cell);
+function cellClicked(evt) { // function to listen for when a cell is clicked and changes the color
+  //console.log(evt.target.id + " clicked");
+  const chosenCell = evt.target.id;
   var chosenColor = colorInput.value;
-  chosenCell.setAttribute('style', 'background-color:' + chosenColor );
-  console.log(chosenColor + "clicked for " + tdID);
+  evt.target.setAttribute('style','background-color : ' + chosenColor);
+  //console.log(chosenColor + " clicked for " + chosenCell);
 }
+
 function makeGrid() {
-// Your code goes here!
-
-
 // Select size input
-
-  console.log("makeGrid entered");
+//  console.log("makeGrid entered");
   var tableWidth = dataSet.get("0")
   var tableHeight = dataSet.get("1");
   const gridTable = document.createElement('table');
 
-
-  workSpace.appendChild(gridTable);
-
-
 // creating the grid works.
-// table height and width works.
 // need to clear old table to build new one
-// need to select cell for colorChanger
 
   for (var i = 1;i <= tableHeight;++i) {
     // create new row
@@ -46,16 +43,9 @@ function makeGrid() {
     newRow.setAttribute('id',rowID);
     for (var j = 1;j <= tableWidth;++j) {
       // create new cell
-      var tdID = "tdx" + i + "y" + j;
+      tdID = "tdx" + i + "y" + j;
       const newCell = document.createElement('td');
       newCell.setAttribute('id',tdID);
-      newCell.addEventListener('click', function() {
-        console.log(tdID + " clicked");
-        var chosenColor = colorInput.value;
-        var chosenCell = document.getElementById(tdID);
-        colorChanger(chosenCell);
-
-      });
       // append to existing row
       newRow.appendChild(newCell);
       }
@@ -63,37 +53,32 @@ function makeGrid() {
     gridTable.appendChild(newRow);
   }
 
+  if(firstRun == 0) {
+    gridSpace.appendChild(gridTable);
+    firstRun = 1;
+  } else {
+    gridSpace.firstChild.replaceWith(gridTable);
+  }
 }
 // on submit, create grid and add to table
 form.addEventListener('submit',(e) => {
   // on form submission, prevent default
   e.preventDefault();
-
-  // construct a FormData object, which fires the formdata event
   new FormData(form);
 });
 
+workSpace.addEventListener('click', cellClicked);
+
 form.addEventListener('formdata', (e) => {
-  console.log('formdata fired');
-  dataSet.delete('value');
-
-
+  // console.log('formdata fired');
+  dataSet.delete('value'); // deletes old values from dataSet
   // Get the form data from the event object
   let data = e.formData;
   var i = 0;
   for (var value of data.values()) {
-    console.log(value);
-    dataSet.append(i,value);
+    //console.log(value);
+    dataSet.set(i,value);
     ++i;
-
-
   }
   makeGrid();
 });
-
-
-// determine which cell was clicked
-// const selectedCell = ???;
-
-// change cell background color to colorInput
-// selectedCell.style.color = colorInput;
