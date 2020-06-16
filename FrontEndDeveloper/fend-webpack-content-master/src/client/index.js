684bdcf2966e69fddf6d350135cd8e93867516fe
nightsyde <nightside@iobbow.org>
2020-06-16 19:59:26 -0400

    update: M2L2Ex13

--- FrontEndDeveloper/fend-webpack-content-master/src/client/index.js
+++ FrontEndDeveloper/fend-webpack-content-master/src/client/index.js
@@ -1,5 +1,7 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+//import css from 'base.css';
+
 
 console.log(checkForName);
 
--- FrontEndDeveloper/fend-webpack-content-master/webpack.config.js
+++ FrontEndDeveloper/fend-webpack-content-master/webpack.config.js
@@ -1,17 +1,49 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: ".\\src\\client\\index.js",
   module: {
     rules: [
       {
-        test: '/\.js$/',
+        enforce: 'pre',
+        test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
-        
-      }
+        loader: 'eslint-loader',
+        options: {
+          quiet: true,
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.json5$/i,
+        loader: 'json5-loader',
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: ".\\src\\client\\views\\index.html",
+    })
+  ]
 }
