8ef2b8e007973de8d44b143ba838f419799a693d
nightsyde <nightside@iobbow.org>
2020-06-16 20:47:40 -0400

    Update: M4L2End
    Webpack lesson complete

--- FrontEndDeveloper/fend-webpack-content-master/webpack.dev.js
+++ FrontEndDeveloper/fend-webpack-content-master/webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: ".\\src\\client\\index.js",
@@ -44,6 +45,17 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: ".\\src\\client\\views\\index.html",
-    })
+    }),
+    new CleanWebpackPlugin({
+        // Simulate the removal of files
+        dry: true,
+        // Write Logs to Console
+        verbose: true,
+        // Automatically remove all unused webpack assets on rebuild
+        cleanStaleWebpackAssets: true,
+        protectWebpackAssets: false
+    }),
+    new BundleAnalyzerPlugin(),
+
   ]
 }
--- FrontEndDeveloper/fend-webpack-content-master/webpack.prod.js
+++ FrontEndDeveloper/fend-webpack-content-master/webpack.prod.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: ".\\src\\client\\index.js",
@@ -44,6 +45,15 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: ".\\src\\client\\views\\index.html",
+    }),
+    new CleanWebpackPlugin({
+        // Simulate the removal of files
+        dry: true,
+        // Write Logs to Console
+        verbose: true,
+        // Automatically remove all unused webpack assets on rebuild
+        cleanStaleWebpackAssets: true,
+        protectWebpackAssets: false
     })
   ]
 }
